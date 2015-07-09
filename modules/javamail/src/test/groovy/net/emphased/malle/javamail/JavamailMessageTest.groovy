43fc20ad0f891a9ddc6c010d6fcfa95f6a843dc9
pingw33n <pingw33n@emphased.net>
2015-07-10 00:23:51 +0300

    Javamail tests

---
+++ modules/javamail/src/test/groovy/net/emphased/malle/javamail/JavamailMessageTest.groovy
@@ -0,0 +1,63 @@
+package net.emphased.malle.javamail
+import spock.lang.Specification
+
+import javax.mail.Message
+import javax.mail.internet.InternetAddress
+import javax.mail.internet.MimeMessage
+
+class JavamailMessageTest extends Specification {
+
+    def javamail = new Javamail()
+
+    def "creates non-multipart MimeMessage with headers and plain text"() {
+        given:
+
+        JavamailMessage m = javamail.createMailMessage(false)
+
+        when:
+
+        m.from("from@example.com")
+                .to("to@example.com")
+                .subject("This is a subject")
+                .plain("Hello from Malle")
+
+        then:
+
+        MimeMessage mm = m.getMimeMessage()
+
+        mm.getFrom().length == 1
+        assertInternetAddress("from@example.com", null, mm.getFrom()[0])
+
+        mm.getRecipients(Message.RecipientType.TO).length == 1
+        assertInternetAddress("to@example.com", null, mm.getRecipients(Message.RecipientType.TO)[0])
+
+        mm.getSubject() == "This is a subject"
+        mm.getContent() == "Hello from Malle"
+
+        MimeMessageRawMatcher.assertMatch("non_mp_from_to_subject_text.eml", mm)
+    }
+
+    def "creates multipart MimeMessage with headers and plain/html text"() {
+        given:
+
+        JavamailMessage m = javamail.createMailMessage(true)
+
+        when:
+
+        m.from("from@example.com")
+                .to("to@example.com")
+                .subject("This is a subject")
+                .plain("Hello from Malle /plain")
+                .html("Hello from Malle /html")
+
+        then:
+
+        MimeMessageRawMatcher.assertMatch("mp_headers_plain_html.eml", m.getMimeMessage())
+    }
+
+    void assertInternetAddress(String address, String personal, actual) {
+        assert actual instanceof InternetAddress
+        assert actual.getAddress() == address
+        assert actual.getPersonal() == personal
+    }
+}
---
+++ modules/javamail/src/test/java/net/emphased/malle/javamail/MimeMessageRawMatcher.java
@@ -0,0 +1,166 @@
+package net.emphased.malle.javamail;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class MimeMessageRawMatcher {
+
+    public static void assertMatch(Reader expected, Reader actual) throws IOException {
+        int line = 0;
+        List<String> vars = new ArrayList<String>();
+        while (true) {
+            line++;
+            Line eline = readLine(expected);
+            Line aline = readLine(actual);
+
+            switch (aline.getTerminator()) {
+                case CRLF:
+                case EOF:
+                    break;
+                default:
+                    throw new AssertionError("Line " + line + " terminator mismatch: CRLF expected but " + aline.getTerminator() + " found");
+            }
+
+            String elineStr = replace(eline.toString(), vars);
+            Pattern epattern = toPattern(elineStr);
+
+            Matcher m = epattern.matcher(aline.toString());
+            if (!m.matches()) {
+                throw new AssertionError("Line " + line + " mismatch: '" + elineStr + "' expected but '" + aline + " found");
+            }
+
+            for (int i = 0; i < m.groupCount(); i++) {
+                vars.add(m.group(i + 1));
+            }
+
+            if (aline.getTerminator() == Line.Terminator.EOF) {
+                break;
+            }
+        }
+    }
+
+    public static void assertMatch(String expectedResource, MimeMessage actual) throws IOException, MessagingException {
+        InputStream is = MimeMessageRawMatcher.class.getClassLoader().getResourceAsStream(expectedResource);
+        if (is == null) {
+            throw new IOException("Couldn't find resource: " + expectedResource);
+        }
+        try {
+            ByteArrayOutputStream baos = new ByteArrayOutputStream();
+            actual.writeTo(baos);
+            {
+                FileOutputStream os = new FileOutputStream("/tmp/test2.eml");
+                os.write(baos.toByteArray());
+                os.close();
+            }
+            assertMatch(new BufferedReader(new InputStreamReader(is, "ISO-8859-1")),
+                    new BufferedReader(new InputStreamReader(new ByteArrayInputStream(baos.toByteArray()), "ISO-8859-1")));
+        } finally {
+            is.close();
+        }
+    }
+
+    private static Pattern toPattern(String s) {
+        StringBuilder r = new StringBuilder(s.length() + 100);
+
+        int pos = 0;
+        while (true) {
+            int rePos = s.indexOf('~', pos);
+            if (rePos == -1) {
+                break;
+            }
+            r.append(Pattern.quote(s.substring(pos, rePos)));
+            pos = rePos + 1;
+            rePos = s.indexOf('~', pos);
+            if (rePos == -1) {
+                rePos = s.length();
+            }
+            r.append(Pattern.compile(s.substring(pos, rePos)));
+            pos = Math.min(s.length(), rePos + 1);
+        }
+        r.append(Pattern.quote(s.substring(pos)));
+
+        return Pattern.compile(r.toString());
+    }
+
+    private static String replace(String s, List<String> vars) {
+        for (int i = 0; i < vars.size(); i++) {
+            s = s.replace("${" + i + "}", vars.get(i));
+        }
+        return s;
+    }
+
+    private static class Line {
+        enum Terminator {
+            CR, LF, CRLF, EOF
+        }
+
+        private final String content;
+        private final Terminator terminator;
+
+        public Line(CharSequence content, Terminator terminator) {
+            this.content = content.toString();
+            this.terminator = terminator;
+        }
+
+        public Terminator getTerminator() {
+            return terminator;
+        }
+
+        @Override
+        public String toString() {
+            return content;
+        }
+    }
+
+    private static Line readLine(Reader reader) throws IOException {
+        StringBuilder r = new StringBuilder(1024);
+        while (true) {
+            Line.Terminator t = null;
+            int c = reader.read();
+            switch (c) {
+                case -1:
+                    return new Line(r, Line.Terminator.EOF);
+                case '\r':
+                    reader.mark(1);
+                    c = reader.read();
+                    switch (c) {
+                        case -1:
+                            reader.reset();
+                            return new Line(r, Line.Terminator.CR);
+                        case '\n':
+                            t = Line.Terminator.CRLF;
+                            break;
+                        default:
+                            reader.reset();
+                            t = Line.Terminator.LF;
+                    }
+                    break;
+                case '\n':
+                    t = Line.Terminator.LF;
+                default:
+                    r.append((char) c);
+            }
+
+            if (t != null) {
+                reader.mark(1);
+                c = reader.read();
+                if (c != '\t') {
+                    reader.reset();
+                    return new Line(r, t);
+                }
+                do {
+                    reader.mark(1);
+                    c = reader.read();
+                } while (c == '\t');
+                reader.reset();
+            }
+        }
+    }
+
+    private MimeMessageRawMatcher() {}
+}
