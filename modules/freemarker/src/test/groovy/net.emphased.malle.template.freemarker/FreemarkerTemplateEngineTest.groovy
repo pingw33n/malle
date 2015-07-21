aa5a6d9302e2fbc9cb4096c0937ee8731951a30a
pingw33n <pingw33n@emphased.net>
2015-07-21 22:18:58 +0300

    FreemarkerTemplateEngineTest

---
+++ modules/freemarker/src/test/groovy/net.emphased.malle.template.freemarker/FreemarkerTemplateEngineTest.groovy
@@ -0,0 +1,50 @@
+package net.emphased.malle.template.freemarker
+import freemarker.template.Configuration
+import freemarker.template.TemplateExceptionHandler
+import net.emphased.malle.Encoding
+import net.emphased.malle.MailMock
+import net.emphased.malle.MailMockAssert
+import net.emphased.malle.support.InputStreamSuppliers
+import org.junit.Before
+import org.junit.Test
+
+class FreemarkerTemplateEngineTest {
+
+    FreemarkerTemplateEngine t
+
+    @Before
+    void setUp() {
+        Configuration fc = new Configuration(Configuration.VERSION_2_3_22)
+        fc.setTemplateExceptionHandler(TemplateExceptionHandler.RETHROW_HANDLER)
+        fc.setClassForTemplateLoading(getClass(), "/")
+        fc.setDefaultEncoding("UTF-8")
+
+        t = new FreemarkerTemplateEngine()
+                .withConfiguration(fc)
+    }
+
+    @Test
+    void "applies template"() {
+        MailMock actual = (MailMock) new MailMock(true)
+                .withTemplateEngine(t)
+                .template("applies template.ftl",
+                "to", "to@example.com",
+                "toPersonal", "Unicode ♡ Malle")
+
+        MailMock expected = (MailMock) new MailMock(true)
+                .bodyEncoding(Encoding.BASE64)
+                .from("from@example.com", null)
+                .to("to@example.com", "Unicode ♡ Malle")
+                .cc("cc@example.com", "Malle ♡ Unicode")
+                .cc("cc2@example.com,\n" +
+                "    \"CC 3\" <cc3@example.com>,\n" +
+                "    \"♡ Unicode ♡\" <cc4@example.com>")
+                .plain("    Plain hello ☺")
+                .html("<p>Html hello ☺</p>")
+                .attachment(InputStreamSuppliers.bytes("    Hello there ✌".getBytes("UTF-8")), "embedded.txt")
+                .attachment(InputStreamSuppliers.resource("classpath.txt"), "classpath.txt")
+                .inline(InputStreamSuppliers.resource("image1.png"), "inline.png")
+
+        MailMockAssert.assertThat(actual).isEqualTo(expected)
+    }
+}
---
+++ modules/freemarker/src/test/resources/applies template.ftl
@@ -0,0 +1,30 @@
+<@mail cmd='settings' body_encoding='base64'/>
+
+<@mail cmd='from' address="from@example.com"/>
+
+<@mail cmd='to' address="${to}" personal="${toPersonal}"/>
+
+<@mail cmd='cc' address="cc@example.com" personal="Malle ♡ Unicode"/>
+
+<@mail cmd='cc'>
+    cc2@example.com,
+    "CC 3" <cc3@example.com>,
+    "♡ Unicode ♡" <cc4@example.com>
+</@mail>
+
+<@mail cmd='plain'>
+    Plain hello ☺
+</@mail>
+
+<@mail cmd='html'>
+    <p>Html hello ☺</p>
+</@mail>
+
+<@mail cmd='attachment' name="embedded.txt">
+    Hello there ✌
+</@mail>
+
+<@mail cmd='attachment' name="classpath.txt" resource="classpath.txt"/>
+
+<@mail cmd='inline' id="inline.png" resource="image1.png"/>
+
