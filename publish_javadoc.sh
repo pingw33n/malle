68be2391ffbb5a099b3ef5fd1660ecd996527dd3
pingw33n <pingw33n@emphased.net>
2015-07-30 14:07:31 +0300

    Wrap the vars with curly braces

--- publish_javadoc.sh
+++ publish_javadoc.sh
@@ -15,21 +15,21 @@ DIR_PREFIX=$1
 GH_TOKEN=$2
 
 GH_REPO=pingw33n/malle
-JAVADOC_DIR=javadoc-$DIR_PREFIX
+JAVADOC_DIR=javadoc-${DIR_PREFIX}
 
 if [ "$GH_TOKEN" == "" ]; then
     GH_URL=git@github.com:pingw33n/malle.git
 else
-    GH_URL=https://$GH_TOKEN@github.com/$GH_REPO
+    GH_URL=https://${GH_TOKEN}@github.com/${GH_REPO}
 fi
 
 mvn clean javadoc:aggregate -Pjavadoc
 APIDOCS_DIR=`pwd`/target/site/apidocs
 [ -d /tmp/gh-pages ] && rm -rf /tmp/gh-pages
-git clone --branch=gh-pages $GH_URL /tmp/gh-pages
+git clone --branch=gh-pages ${GH_URL} /tmp/gh-pages
 cd /tmp/gh-pages
-[ -d "$JAVADOC_DIR" ] && git rm -rf $JAVADOC_DIR && rm -rf $JAVADOC_DIR > /dev/null
-cp -r $APIDOCS_DIR $JAVADOC_DIR
-git add -f $JAVADOC_DIR
+[ -d "$JAVADOC_DIR" ] && git rm -rf ${JAVADOC_DIR} && rm -rf ${JAVADOC_DIR} > /dev/null
+cp -r ${APIDOCS_DIR} ${JAVADOC_DIR}
+git add -f ${JAVADOC_DIR}
 git commit -m "auto-publish javadoc" > /dev/null
 git push -f origin gh-pages
