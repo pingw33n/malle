dcfa014b5f6392133000a4c78890508261ddaeea
pingw33n <pingw33n@emphased.net>
2015-07-30 13:59:14 +0300

    Publish JavaDocs with 1.8

--- .travis/javadoc.sh
+++ .travis/javadoc.sh
@@ -1,27 +1,28 @@
 #!/bin/bash
 #########################################################################################################
-# Deploys the artifacts.
+# Publishes the JavaDocs.
 #
 # Expects the following environment vars to be present:
 # EXPECTED_REPO_SLUG    - Expected repository name.
 # EXPECTED_BRANCH       - Expected branch name.
-# DEPLOY_JDK            - JDK for which to deploy.
-# OSSRH_USERNAME        - Sonatype username.
-# OSSRH_PASSWORD        - Sonatype password.
+# JAVADOC_JDK           - JDK which to generate JavaDocs with.
+# GH_TOKEN              - GitHub personal token.
 #########################################################################################################
 
 set -e
 
 if [ "$TRAVIS_REPO_SLUG" != "$EXPECTED_REPO_SLUG" ]; then
     echo "Skipping snapshot deployment: wrong repository. Expected '$EXPECTED_REPO_SLUG' but was '$TRAVIS_REPO_SLUG'."
-elif [ "$TRAVIS_JDK_VERSION" != "$DEPLOY_JDK" ]; then
-    echo "Skipping snapshot deployment: wrong JDK. Expected '$DEPLOY_JDK' but was '$TRAVIS_JDK_VERSION'."
+elif [ "$TRAVIS_JDK_VERSION" != "$JAVADOC_JDK" ]; then
+    echo "Skipping snapshot deployment: wrong JDK. Expected '$JAVADOC_JDK' but was '$TRAVIS_JDK_VERSION'."
 elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
     echo "Skipping snapshot deployment: this is a pull request."
 elif [ "$TRAVIS_BRANCH" != "$EXPECTED_BRANCH" ]; then
     echo "Skipping snapshot deployment: wrong branch. Expected '$EXPECTED_BRANCH' but was '$TRAVIS_BRANCH'."
 else
-    echo "Deploying snapshot..."
-    mvn deploy --settings=.travis/settings.xml -Psnapshot -DskipTests
-    echo "Snapshot deployed!"
+    echo "Publishing JavaDocs..."
+    git config --global user.email "travis@travis-ci.org"
+    git config --global user.name "travis-ci"
+    chmod +x ./publish_javadoc.sh && ./publish_javadoc.sh ${EXPECTED_BRANCH} ${GH_TOKEN}
+    echo "JavaDocs published!"
 fi
